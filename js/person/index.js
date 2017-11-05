54d9884664643bc5456bb4b002e677d869804794
Mehmet Dogan <info@intelligentworks.ch>
2017-11-05 17:52:55 +0100

    add person

--- js/person/index.js
+++ js/person/index.js
@@ -1,2 +1,38 @@
 
-
+let person = 
+   {
+    "id": 45,
+    "firstName": "Mehmet",
+    "lastName": "B.",
+    "gender": "M",
+    "birth": new Date(1978, 7, 5),
+    "origin": "TR",
+    "children": [
+        {
+            "firstName": "Büsra",
+            "gender": "M",
+            "birth": new Date(2007, 5, 5),
+        },
+        {
+            "firstName": "Serhat",
+            "gender": "F",
+            "birth": new Date(2009, 6, 5),
+        },
+        {
+            "firstName": "Elif",
+            "gender": "F",
+            "birth": new Date(20011, 7, 5),
+        },
+    ],
+    "addres": {
+        "street": "Matternhornstr. 123",
+        "zip": "8604",
+        "city": "Volketswil",
+        "country": "CH"
+    },
+    "languages": ["EN", "DE", "TR"],
+    "salary": 83000.00,
+    "friends": [
+        
+    ]
+}
